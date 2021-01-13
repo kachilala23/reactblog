94fb628088e9047ac5a94f032a7b88174e73357e
kachilala <kachilala23@gmail.com>
2021-01-13 01:42:35 +0100

    showed how to handle event and css with nested component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  let title = 'Welcome to my blog'
-  
   return (
     <div className="App">
-     <div className="content">
-       <h1>{ title }</h1>
-     </div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
